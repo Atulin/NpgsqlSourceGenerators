c087332e20acd1b1affe68b917bce5cba35df416
Atulin <11233299+Atulin@users.noreply.github.com>
2024-01-09 09:11:07 +0100

    Betterment - Added a demo project (doesn't work lmao) - Better way to collect the enums with a given attribute... hopefully, since neither demo nor tests work

---
+++ NpgsqlSourceGenerators.Demo/Program.cs
@@ -0,0 +1,13 @@
+﻿using NpgSqlEnumGenerators;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+[PostgresEnum]
+enum Directions
+{
+	North,
+	West,
+	South,
+	East
+}
