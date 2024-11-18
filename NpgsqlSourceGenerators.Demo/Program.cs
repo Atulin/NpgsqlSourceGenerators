5b474e3f2fa7e2a9fd84cb2709f69a258ff143fa
Atulin <11233299+Atulin@users.noreply.github.com>
2024-11-18 19:40:00 +0100

    Updated to Npgsql.EF 9.0.0 - Added additional attributes to generated code to make it nicer to work with

--- NpgsqlSourceGenerators.Demo/Program.cs
+++ NpgsqlSourceGenerators.Demo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Npgsql;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Infrastructure;
 using NpgSqlGenerators;
 
 Console.WriteLine("Hello, World!");
@@ -9,6 +10,9 @@ dataSourceBuilder.MapPostgresEnums();
 
 var modelBuilder = new ModelBuilder();
 modelBuilder.RegisterPostgresEnums();
+
+var optionsBuilder = new NpgsqlDbContextOptionsBuilder(new DbContextOptionsBuilder());
+optionsBuilder.MapPostgresEnums();
 
 
 [PostgresEnum(Name = "DoW")]
